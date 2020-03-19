d38425f19dd4a6cb207c968fc24a3c9ac39dcdbe
vanceleon <vanceleon44@gmail.com>
2020-03-18 23:28:17 -0400

    server running on 5000

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps);
 
 app.use(errorHandler);
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
